4313f73fe7064c1ca925a6d24954dd255ff28d77
Yonatan Rojas <yonatan@bedu.org>
2018-04-08 00:50:39 -0500

    another test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { WeatherService } from "./weather.service";
 import { Chart } from "chart.js";
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
